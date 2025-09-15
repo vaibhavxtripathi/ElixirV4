4c6ffa67ec48d6363dbd75d51b4618c6a554b1da
Vaibhav Tripathi <vaibhavxtripathi@gmail.com>
2025-09-15 15:14:43 +0530

    fix: update Scale component background and border styles for improved visual consistency

--- src/components/scale.tsx
+++ src/components/scale.tsx
@@ -5,13 +5,13 @@ export const Scale = ({ className }: { className?: string }) => {
   return (
     <div
       className={cn(
-        "bg-none absolute inset-0 z-10 m-auto h-full w-full rounded-lg border border-gray-300 dark:border-gray-700",
+        "absolute inset-0 z-10 m-auto h-full w-full rounded-lg dark:border border-white/20 bg-neutral-900 bg-[image:repeating-linear-gradient(315deg,_var(--pattern-fg)_0,_var(--pattern-fg)_1px,_transparent_0,_transparent_50%)] bg-[size:10px_10px]",
         className
       )}
       style={{
         background: ` 
-                     linear-gradient(45deg, transparent 45%, #9ca3af 45%, #9ca3af 55%, transparent 55%)`,
-        backgroundSize: "6px 6px",
+                     linear-gradient(45deg, transparent 45%, #2B2B2B 45%, #2B2B2B 55%, transparent 55%)`,
+        backgroundSize: "10px 10px",
       }}
     ></div>
   );
