8a28a86e4001498de357cb290b259d65063c67e4
Vaibhav Tripathi <vaibhavxtripathi@gmail.com>
2025-09-15 15:14:57 +0530

    feat: integrate Scale component into tech card for enhanced visual effects and layout consistency

--- src/components/tech-card.tsx
+++ src/components/tech-card.tsx
@@ -3,6 +3,7 @@ import { motion, useMotionValue, useSpring, useTransform } from "motion/react";
 import React, { useRef } from "react";
 import { DivideX } from "./divide";
 import { cn } from "@/lib/utils";
+import { Scale } from "@/components/scale";
 
 const springConfig = {
   stiffness: 300,
@@ -73,7 +74,7 @@ export const Card = ({
       className={cn("relative h-full text-xs", className)}
       ref={ref}
     >
-      <div className="absolute inset-0 z-10 m-auto h-full w-full rounded-lg border border-(--pattern-fg) bg-white bg-[image:repeating-linear-gradient(315deg,_var(--pattern-fg)_0,_var(--pattern-fg)_1px,_transparent_0,_transparent_50%)] bg-[size:10px_10px] bg-fixed dark:bg-neutral-900"></div>
+      <Scale />
       <div className="absolute inset-x-0 -top-1.5 mx-auto size-4 rounded-full border-2 border-gray-300 bg-white dark:border-neutral-700 dark:bg-neutral-900"></div>
 
       <motion.div
@@ -85,7 +86,7 @@ export const Card = ({
         }}
         className="shadow-aceternity relative z-20 flex w-54 shrink-0 flex-col items-start rounded-lg bg-white dark:bg-neutral-900 p-5"
       >
-        <div className="flex w-full items-center justify-between p-2 md:p-4">
+        <div className="flex w-full items-center justify-between">
           <div className="flex items-center gap-2 font-medium">
             {logo}
             {title}
@@ -101,7 +102,7 @@ export const Card = ({
             tone === "danger" &&
               "border-orange-500 bg-red-50 text-orange-500 dark:bg-red-50/10 dark:text-red-500",
             tone === "success" &&
-              "border-neutral-500 bg-neutral-50 text-neutral-500 dark:bg-neutral-50/10 dark:text-neutral-500",
+              "border-neutral-500 bg-neutral-50 text-neutral-500 dark:bg-neutral-50/10 dark:text-neutral-500"
           )}
         >
           {cta}
